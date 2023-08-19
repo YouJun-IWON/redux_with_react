4d249f1d67cdf8d74696de4126b4fc3460f2967b
ReasonJun <117330708+YouJun-IWON@users.noreply.github.com>
2023-08-19 16:03:43 +0900

    feat: :sparkles: set middleware and thunk

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import counter from './reducers';
 import rootReducer from './reducers';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
 
 
@@ -13,21 +15,34 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const store = createStore(rootReducer);
 
-store.dispatch({
-  type: "ADD_TODO",
-  text: "USE REDUX"
-})
+
+const loggerMiddleware = (store: any) => (next: any) => (action: any) => {
+  console.log("store", store)
+  console.log('action', action);
+  next(action);
+}
+
+const middleware = applyMiddleware(thunk,loggerMiddleware);
+
+
+const store = createStore(rootReducer, middleware);
+
+// store.dispatch({
+//   type: "ADD_TODO",
+//   text: "USE REDUX"
+// })
 
 console.log('store.getState', store.getState());
 
 const render = () => root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App value={store.getState()}
     onIncrement={()=> store.dispatch({type:'INCREMENT'})}
     onDecrement={()=> store.dispatch({type:'DECREMENT'})}
     />
+    </Provider>
   </React.StrictMode>
 );
 
--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -1,11 +1,14 @@
 import { combineReducers } from 'redux';
 import counter from './counter';
 import todos from './todos';
-
+import posts from './post';
 
 const rootReducer = combineReducers({
   counter,
-  todos
+  todos,
+  posts
 })
 
 export default rootReducer;
+
+export type RootState = ReturnType<typeof rootReducer>
